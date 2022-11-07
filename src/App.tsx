4a4342f1c04b6edebdd39c627dda8e6fdec356d5
Bartłomiej_Holik <bartlomiej.holik@altconnect.pl>
2022-11-07 15:20:10 +0100

    add header/logo to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/header';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Header />
   );
 }
 
---
+++ src/components/Header/header.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style.css';
+
+const Header = () => {
+
+
+    return(
+        <div className='header'>
+            <div className='logo' data-text="CINEMA">CINEMA</div>
+            <div className='login'>Login</div>
+            <div className='register'>Sign Up</div>
+        </div>
+    )
+};
+
+export default Header;
+
