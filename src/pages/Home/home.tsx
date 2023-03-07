b0dd297a2df2f5c36e506446f8894666f47ae57b
DrumskY <bartekholikti1@gmail.com>
2023-03-07 15:22:23 +0100

    clean code

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,5 +1,4 @@
 import './style.css';
-import Header from '../../components/header/header';
 import MovieHeader from '../../components/movie-baner';
 import MoviesList from '../../components/movies-list';
 import MoviesListByRating from '../../components/movies-list/searchByRating';
